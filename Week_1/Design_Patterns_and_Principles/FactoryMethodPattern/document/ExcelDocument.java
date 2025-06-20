a582b6fee2623b8b35371fa16325ea42aa2c9b81
stanispaul <125119734+stanispaul@users.noreply.github.com>
2025-06-20 13:30:25 +0530

    Add files via upload

---
+++ Week_1/Design_Patterns_and_Principles/FactoryMethodPattern/document/ExcelDocument.java
@@ -0,0 +1,8 @@
+// package document;
+
+public class ExcelDocument implements Document {
+    public void open() {
+        System.out.println("Openeing Excel Document...");
+    }
+
+}
---
+++ Week_1/Design_Patterns_and_Principles/FactoryMethodPattern/document/ExcelDocumentFactory.java
@@ -0,0 +1,7 @@
+
+public class ExcelDocumentFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new ExcelDocument();
+    }
+
+}
